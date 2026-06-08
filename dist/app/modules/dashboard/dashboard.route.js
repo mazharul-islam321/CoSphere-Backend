c3555157f17103333a2f51a98b2cc08a30a11645
mazharul-islam321 <mazharulislam.lu2016@gmail.com>
2026-06-09 00:26:40 +0600

    update

--- dist/app/modules/dashboard/dashboard.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DashboardRoutes = void 0;
-const express_1 = require("express");
-const dashboard_controller_1 = require("./dashboard.controller");
-const auth_1 = require("../../middlewares/auth");
-const router = (0, express_1.Router)();
-router.get('/', auth_1.authenticateJWT, dashboard_controller_1.DashboardController.getDashboardStats);
-exports.DashboardRoutes = router;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,17 @@
 /// <reference types="node" />
+import 'dotenv/config';
 import { PrismaClient, Role, ProjectStatus, TaskPriority, TaskStatus } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 import * as bcrypt from 'bcryptjs';
 
-const prisma = new PrismaClient();
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   console.log('Seeding database...');
